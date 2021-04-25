403c3519b1e1337517b3a617bd04c5dccad28a56
David Manor <david@dolphextechnologies.com>
2021-04-25 23:20:34 +0100

    [add] search button to search Input

--- src/app/components/search/search.jsx
+++ src/app/components/search/search.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import {Input} from '@chakra-ui/react'
+import React from "react";
+import { Input, InputRightElement, InputGroup, Button } from "@chakra-ui/react";
 
 const Search = () => {
-    const [value, setValue] = React.useState("")
-    const handleChange = (event) => setValue(event.target.value)
+  const [value, setValue] = React.useState("");
+  const handleChange = (event) => setValue(event.target.value);
 
   return (
-    <>
+    <InputGroup width="xs">
       <Input
         value={value}
+        variant="filled"
         onChange={handleChange}
         placeholder="Search plant.."
-        size="sm"
+        size="lg"
+        // Width="xs"
       />
-    </>
-  )
-}
+      <InputRightElement width="4.5rem">
+        <Button  size="lg" margin=""  >
+        {'Search'}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+};
 
-export default Search
+export default Search;
