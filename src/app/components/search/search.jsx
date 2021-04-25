1da3f6d6df2a4c160a4c532e12a2abc80716677e
David Manor <david@dolphextechnologies.com>
2021-04-25 23:39:23 +0100

    improve search bar

--- src/app/components/search/search.jsx
+++ src/app/components/search/search.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { Input, InputRightElement, InputGroup, Button } from "@chakra-ui/react";
+import {
+  Input,
+  InputRightElement,
+  InputGroup,
+  Button,
+  Center,
+} from "@chakra-ui/react";
 
 const Search = () => {
   const [value, setValue] = React.useState("");
   const handleChange = (event) => setValue(event.target.value);
 
   return (
-    <InputGroup width="xs">
-      <Input
-        value={value}
-        variant="filled"
-        onChange={handleChange}
-        placeholder="Search plant.."
-        size="lg"
-        // Width="xs"
-      />
-      <InputRightElement width="4.5rem">
-        <Button  size="lg" margin=""  >
-        {'Search'}
-        </Button>
-      </InputRightElement>
-    </InputGroup>
+    <Center  marginTop="10">
+      <InputGroup width="xs">
+        <Input
+          value={value}
+          variant="filled"
+          onChange={handleChange}
+          placeholder="Search plant.."
+          size="lg"
+          Width="xs"
+        />
+        <InputRightElement width="4.5rem">
+          <Button size="lg" margin="">
+            {"Search"}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </Center>
   );
 };
 
