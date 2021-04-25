6fee895eb91b6c7104affcc1a458ec4b1bbeaab1
David Manor <david@dolphextechnologies.com>
2021-04-25 20:21:34 +0100

    [add] basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Logo from "./app/components/logo/logo";
+import Home from "./app/pages/home/home";
 
 function App() {
   return (
     <div className="App">
-
+      <Logo />
+      <Switch>
+        <Route path="/app">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
