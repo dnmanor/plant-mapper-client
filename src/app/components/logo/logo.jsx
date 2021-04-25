8ec57cece6b634cd17c3fa7be83c496fbf1810c6
David Manor <david@dolphextechnologies.com>
2021-04-25 22:12:07 +0100

    [add] logo component

---
+++ src/app/components/logo/logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logo = () => {
+
+    const name = 'plant mapper'
+
+    return (
+        <div>
+            <p className="logo" style={{textTransform:'uppercase'}}> {name}</p>
+        </div>
+    )
+}
+
+export default Logo
